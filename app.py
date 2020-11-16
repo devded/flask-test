850735ca91ac381bfe3ce3bb98aee6a1dad91709
smdedar <thededar@gmail.com>
2020-11-16 20:04:06 +0600

    update

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-my_awesome_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@my_awesome_app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/')
+def index():
+    return "<h1> Deployed to Heroku</h1>"
 
 
-if __name__ == '__main__':
-    my_awesome_app.run()
+if __name__ == "__main__":
+    app.run()
